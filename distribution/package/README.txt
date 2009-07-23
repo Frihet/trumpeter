54d3daea760a4cbdfbf8eb29a2ed310932f4c2c6
Reidar Øksnevad <reidar.oksnevad@freecode.no>
2009-07-23 20:06:49 +0200

    Added license files and basic readme-files.

---
+++ distribution/package/README.txt
@@ -0,0 +1,17 @@
+-------------------------------------------------------------------------------
+ Trumpeter
+-------------------------------------------------------------------------------
+
+  ABOUT:
+  =============
+
+  Trumpeter is a stand-alone server that integrates RT (Request
+  Tracker) with an XMPP chat server (used by Jabber, Pidgin, Google
+  Talk, etc.). It regularly queries RT, and posts notification messages
+  in chat rooms based on a configurable set of rules.
+
+
+  See the project page for documentation, source code, bug reports and
+  anything else related to the project:
+
+    https://projects.freecode.no/projects/show/rtnotifier
--- src/main/java/no/freecode/trumpeter/App.java
+++ src/main/java/no/freecode/trumpeter/App.java
@@ -5,6 +5,12 @@ import java.util.Properties;
 
 import no.freecode.trumpeter.xmpp.XmppManager;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 import org.apache.log4j.Logger;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,29 +20,51 @@ import org.springframework.core.io.support.PropertiesLoaderUtils;
 /**
  * RT Agent main startup class.
  */
-public class App 
-{
+public class App {
     private static final Logger logger = Logger.getLogger(App.class);
 
-    public static void main( String[] args ) throws InterruptedException
-    {
-        System.out.println("Starting " + getApplicationName() + "...\n--\n");
+    public static void main(String[] args) throws InterruptedException {
+        CommandLineParser parser = new PosixParser();
+
+        // Create the options
+        Options options = new Options();
+        options.addOption("h", "help", false, "print this message");
+//        options.addOption("v", "verbose", false, "print a lot more info");
 
         try {
-            AbstractApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+            // Parse the command line arguments.
+            CommandLine line = parser.parse(options, args);
+            
+            String[] leftoverArgs = line.getArgs();
 
-            // Make sure the application runs the @Destroy methods when exiting.
-            context.registerShutdownHook();
+            if (line.hasOption("help")) {
+                // Generate help statement.
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp(getApplicationName(), options);
 
-            // Invoke the agents at once (i.e. check RT queues), if configured:
-            XmppManager manager = (XmppManager) context.getBean("manager");
-            if (manager.isInvokeOnStartup()) {
-                manager.invokeAgents();
+            } else {
+                System.out.println("Starting " + getApplicationName() + "...\n--\n");
+
+                try {
+                    AbstractApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+
+                    // Make sure the application runs the @Destroy methods when exiting.
+                    context.registerShutdownHook();
+
+                    // Invoke the agents at once (i.e. check RT queues), if configured:
+                    XmppManager manager = (XmppManager) context.getBean("manager");
+                    if (manager.isInvokeOnStartup()) {
+                        manager.invokeAgents();
+                    }
+
+                } catch (NestedRuntimeException e) {
+                    System.err.println("Error: " + e.getMostSpecificCause().getMessage());
+                    logger.error("Error: " + e.getMostSpecificCause().getMessage(), e);
+                }
             }
 
-        } catch (NestedRuntimeException e) {
-            System.err.println("Error: " + e.getMostSpecificCause().getMessage());
-            logger.error("Error: " + e.getMostSpecificCause().getMessage(), e);
+        } catch (ParseException exp) {
+            System.out.println("Unexpected exception: " + exp.getMessage());
         }
     }
 
